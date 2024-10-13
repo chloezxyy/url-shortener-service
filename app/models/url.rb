a9967e94d5172661aa4a67bff63ebd7b06c5f3f5
chloezxyy <44501120+chloezxyy@users.noreply.github.com>
2024-10-13 14:30:38 +0800

    feat: Url model - validation, business logic, and associations

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,30 @@
+# This url class creates an association between the Url model and the Visit model. The Url model has many visits, and when a Url is destroyed, all of its associated visits are destroyed as well. This is a good example of how to use
 class Url < ApplicationRecord
+  # each url is associated with many Visit object, if the url is destroyed, all of its associated visits are destroyed
+  has_many :visits, dependent: :destroy
+
+  # validates the presence of the original_url and the format of the original_url
+  validates :original_url, presence: true, format: URI.regexp(%w[http https])
+  validates :short_url, presence: true, uniqueness: true,  length: { maximum: 15 }
+
+  # before saving the url, generate a short url and fetch the title of the page
+  before_save :generate_short_url, :fetch_title
+
+  private
+
+  # generate a random short url
+  def generate_short_url
+    # Generate a unique identifier
+    unique_id = SecureRandom.random_number(1_000_000)
+    # Convert the unique identifier to a base62 string
+    self.short_url = Base62.encode(unique_id)
+  end
+
+  # fetch the title of the page
+  def fetch_title
+    response = HTTParty.get(self.original_url)
+    self.title = Nokogiri::HTML(response.body).title if response.success?
+  rescue
+    self.title = "Unknown Title"
+  end
 end
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Url, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a url' do
+    url = Url.new(original_url: 'http://www.example.com', short_url: '123223456765434')
+    expect(url).to be_valid
+  end
+
+  it "is invalid if it has does not have a original url" do
+    url = Url.new(
+      short_url: "123223456765434"
+    )
+    expect(url.valid?).to be(false)
+  end
+
+  it 'is invalid without a url' do
+    url = Url.new(original_url: nil)
+    url.valid?
+    expect(url.errors[:original_url]).to include("can't be blank")
+  end
+
+  it 'is invalid with a wrong url' do
+    url = Url.new(original_url: '.example')
+    url.valid?
+    expect(url.errors[:original_url]).to include("is invalid")
+  end
 end
